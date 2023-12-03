189b3060726614360604e275bd66bf185723a1d1
Rener-Pontes <renercpontes@gmail.com>
2023-12-03 00:35:52 -0300

    Create Team class with all getters and setters

---
+++ src/main/java/com/projeto_poo/model/Team.java
@@ -0,0 +1,44 @@
+package com.projeto_poo.model;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author renp
+ */
+public class Team {
+    private String name;
+    private String city;
+    private Trainer trainer;
+
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public Trainer getTrainer() {
+        return trainer;
+    }
+
+    public void setTrainer(Trainer trainer) {
+        this.trainer = trainer;
+    }
+    
+    
+    
+}
